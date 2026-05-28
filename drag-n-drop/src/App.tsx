47a8bd2d303fee711ca2f896efb317d8af4c458e
ZayanMA <zayan.m.a@outlook.com>
2026-05-28 12:28:59 +0100

    add shape and shape location storage

--- drag-n-drop/src/App.tsx
+++ drag-n-drop/src/App.tsx
@@ -3,20 +3,30 @@ import './App.css'
 
 function App() {
 
-  const shapeMapping = {
+  // Mapping between a quadrant id and what shape it represents
+  const shapeMapping = { 
     "top-left": "circle",
     "top-right": "hexagon",
     "bottom-left": "square",
     "bottom-right": "triangle"
   };
 
+  // useState to store and update shape locations
+  const [shapeLocations, setShapeLocation] = useState([
+    {id: "shape-1", quadrant: "top-left"},
+    {id: "shape-2", quadrant: "top-left"},
+    {id: "shape-3", quadrant: "top-left"},
+    {id: "shape-4", quadrant: "top-left"},
+    {id: "shape-5", quadrant: "top-left"},
+  ]);
+
   return (
     <>
       <div className='quadrant-container'>
-        <div className='quadrant'>Top Left</div>
-        <div className='quadrant'>Top Right</div>
-        <div className='quadrant'>Bottom Left</div>
-        <div className='quadrant'>Bottom Right</div>
+        <div className='quadrant' id='top-left'>Top Left</div>
+        <div className='quadrant' id='top-right'>Top Right</div>
+        <div className='quadrant' id='bottom-left'>Bottom Left</div>
+        <div className='quadrant' id='bottom-right'>Bottom Right</div>
       </div>
     </>
   )
